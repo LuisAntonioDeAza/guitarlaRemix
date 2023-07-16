ba17641de4d2cee05c4fbeffc7dbd9657e3c9771
LUIS ANTONIO DE AZA DE JESUS <79406557+LuisAntonioDeAza@users.noreply.github.com>
2023-07-15 20:29:31 -0400

    Update guitarras.server.js

--- app/Models/guitarras.server.js
+++ app/Models/guitarras.server.js
@@ -5,25 +5,25 @@ export async function getGuitar(){
 }
 
 export async function getGuita(url){
-    const respuesta = await fetch(`http://192.168.2.75:1337/api/guitarras?filters[url]=${url}&populate=imagen`);
+    const respuesta = await fetch(`https://strapi-70ig.onrender.com/api/guitarras?filters[url]=${url}&populate=imagen`);
     return await respuesta.json();
 }
 
 
 export async function getPost(){
-    const respuesta =  await fetch('http://192.168.2.75:1337/api/blogs?populate=imagen');
+    const respuesta =  await fetch('https://strapi-70ig.onrender.com/api/blogs?populate=imagen');
     return  await respuesta.json();
    
 }
 
 export async function getposts(url){
     console.log(url);
-    const respuesta = await fetch(`http://192.168.2.75:1337/api/blogs?filters[url]=${url}&populate=imagen`);
+    const respuesta = await fetch(`https://strapi-70ig.onrender.com/api/blogs?filters[url]=${url}&populate=imagen`);
     return await respuesta.json();
 }
 
 export async function getCourse(){
-    const respuesta =  await fetch('http://192.168.2.75:1337/api/curso?populate=imagen');
+    const respuesta =  await fetch('https://strapi-70ig.onrender.com/api/curso?populate=imagen');
     return  await respuesta.json();
    
 }
